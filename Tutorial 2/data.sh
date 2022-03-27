f05927d028ebf468a627d2c687d3f20df9739b8a
arkha98 <arkhasayoga.m@gmail.com>
2022-03-27 14:36:29 +0700

    add tutorial

---
+++ Tutorial 2/data.sh
@@ -0,0 +1,17 @@
+# Mengambil data
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1rFLB_1QpZhKyWFGxqPo5znTEkh7dUhPf' -O dataset.csv
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xEIQwYre1SR71uRdQuez9MDmAUIysvAG' -O stopwordsID.csv
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=19NOzXA8Voturopg_DTuiMns3s4M2IAUz' -O kamus_singkatan.csv
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1VjgivEr1pxyRCuyhVifPnaReFz0yd8Us' -O colloquial-indonesian-lexicon.csv
+
+# Dataset keyword "menguasai dunia", silakan di-uncomment semua baris di bawah ini
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1p2BMF7GDZA6u3wmvbxMO5bs3QxU4m8A_' -O menguasai_dunia.csv
+
+# Dataset keyword "butuh liburan", silakan di-uncomment semua baris di bawah ini
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1aGY6y9vABCKD08MkA6Rwv2FkALHf5VbY' -O butuh_liburan.csv
+
+# Dataset keyword "makan seblak", silakan di-uncomment semua baris di bawah ini
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1x-8ev32dumbu0zENIrEtIJQPfmqKk6H-' -O makan_seblak.csv
+
+# Dataset keyword "nonton kpop", silakan di-uncomment semua baris di bawah ini
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1IzTzONy2Ih5YyXQoMYDd60Nh79YakmQ4' -O nonton_kpop.csv
